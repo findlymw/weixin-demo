a5580bd11efaf72f61fbd6c6b054a266197b30fe
matianyi <findlymw@gmail.com>
2020-06-19 23:25:30 +0800

    添加my页面的布局

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,18 +1,22 @@
 // pages/my/my.js
+let app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.dir(wx.getStorageSync('userInfo'));
+    this.setData({
+      userInfo: wx.getStorageSync('userInfo')
+    });
   },
 
   /**
